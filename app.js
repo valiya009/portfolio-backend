2f235e8e5312e4d90ba14c6f5ba0d004461b4194
Chirag-skillcrafters <chirag.skillcrafters@gmail.com>
2025-07-26 17:05:42 +0530

    update CORS configuration to include DASHBOARD_URL and correct PORTFOLIO_URL

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [`${process.env.PORTFOLIO_URL}`],
+    origin: [`${process.env.PORTFOLIO_URL},${process.env.DASHBOARD_URL}`],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
