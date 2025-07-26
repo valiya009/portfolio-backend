8dd1401dd1797bfeda81e6a6d6cc5ddc0093d0a4
Chirag-skillcrafters <chirag.skillcrafters@gmail.com>
2025-07-26 16:22:48 +0530

    update environment configuration for improved deployment settings

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [`${process.env.DASHBOARD_URL}`, `${process.env.PORTFOLIO_URL}`],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
