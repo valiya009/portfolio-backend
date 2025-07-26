3a2d0c1d95691f9a2b6c4cb19823f744bc2457c0
Chirag-skillcrafters <chirag.skillcrafters@gmail.com>
2025-07-26 22:55:15 +0530

    remove authentication requirement for getting user details

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { deleteMessage, getAllMessages, sendMessage } from "../controller/messageController.js";
+import {
+  deleteMessage,
+  getAllMessages,
+  sendMessage,
+} from "../controller/messageController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage);
 router.delete("/delete/:id", isAuthenticated, deleteMessage);
-router.get("/getall", isAuthenticated, getAllMessages);
+router.get("/getall", getAllMessages);
 
 export default router;
